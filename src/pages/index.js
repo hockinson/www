044f53fc6c1f421d2b3c47bfd490e5a9a11eb992
BetaPictoris <beta@hai.haus>
2023-10-25 12:58:14 -0700

    Added title with changing word and color

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,49 @@
+import { useEffect, useState } from "react";
+
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
+  const words = ["technology", "science", "engineering", "design"];
+  const colors = ["green", "blue", "red", "pink", "orange", "cyan"];
+
+  const [word, setWord] = useState(words[0]);
+  const [color, setColor] = useState(colors[0]);
+
+  useEffect(() => {
+    const current_color = color;
+    const new_colors = colors.filter((c) => c !== current_color);
+
+    const interval = setInterval(() => {
+      const index = words.indexOf(word);
+      if (index === words.length - 1) {
+        setWord(words[0]);
+      } else {
+        setWord(words[index + 1]);
+      }
+      setColor(new_colors[Math.floor(Math.random() * new_colors.length)]);
+    }, 1500);
+    return () => clearInterval(interval);
+  });
+
   return (
     <>
-      <p>Math is the foundation of technology.</p>
-      <p>
-        The Hockinson Math Team applies mathematics to solve real-world
-        problems.
-      </p>
+      <div className={styles.title}>
+        <div className={styles.title__text}>
+          Math is the foundation of{" "}
+          <span
+            className={`${styles.title__text_dyn} ${
+              styles[`title__text_dyn_` + color]
+            }`}
+          >
+            {word}
+          </span>
+          .
+        </div>
+        <div className={styles.title__subtitle}>
+          The Hockinson Math Team applies mathematics to solve real-world
+          problems.
+        </div>
+      </div>
     </>
   );
 }
